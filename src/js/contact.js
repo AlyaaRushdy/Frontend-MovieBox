83d6b84aebcbcab0148073899941724284a313ac
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-08-06 18:57:31 +0300

    conatct page full

--- src/js/contact.js
+++ src/js/contact.js
@@ -2,20 +2,43 @@ emailjs.init({
   publicKey: "KzBHA4lsho3ngb2vi",
 });
 
+const nameInput = document.querySelector("#conatctSenderName");
+const mailInput = document.querySelector("#conatctSenderEmail");
+const messageInput = document.querySelector("#contactSenderMessage");
+
 window.onload = function () {
   document
     .getElementById("contact-form")
     .addEventListener("submit", function (event) {
       event.preventDefault();
-      // these IDs from the previous steps
-      emailjs.sendForm("service_8s8wmll", "template_i52j67k", this).then(
-        () => {
-          console.log("SUCCESS!");
-        },
-        (error) => {
-          console.log("FAILED...", error);
-        }
-      );
+      if (nameInput.value && mailInput.value && messageInput.value) {
+        emailjs.sendForm("service_8s8wmll", "template_i52j67k", this).then(
+          () => {
+            Swal.fire({
+              title: "Success",
+              text: "Your message was sent successfully",
+              icon: "success",
+            });
+            nameInput.value = "";
+            mailInput.value = "";
+            messageInput.value = "";
+          },
+          (error) => {
+            console.log("FAILED...", error);
+            Swal.fire({
+              title: "Error!",
+              text: "There was an error, please try again later",
+              icon: "error",
+            });
+          }
+        );
+      } else {
+        Swal.fire({
+          title: "Error!",
+          text: "all fields must be filled",
+          icon: "error",
+        });
+      }
     });
 };
 
