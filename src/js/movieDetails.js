97c7669800101398c16b30f939b7fcb7bdfef5bb
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-08-09 05:13:02 +0300

    removed all service related functions and the colorschemetoggol  function from movie details

--- src/js/movieDetails.js
+++ src/js/movieDetails.js
@@ -1,104 +1,16 @@
+import { getMovieDetails } from "./services.js";
+
+import {
+  languageCodeToName,
+  countryCodeToName,
+  genreIdToName,
+} from "./services.staticLists.js";
+
 const cardsContainer = document.querySelector(".cards-container");
 const detailsDiv = document.querySelector(".details");
+const similarMoviesSection = document.querySelector(".similar-movies");
 
-// get request headers
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNTA2ZGRkNjExM2Q5MDgzZWU5MjdlM2VhNDdjYzgyNSIsIm5iZiI6MTcyMDQ0NTk3NC40NDA1NzEsInN1YiI6IjY2OGE2YmU0NWUxZGYzYWQzMTU3ZWFlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yF88ur3H_q8MhuIdcuuTCCOOX1OxzXz4DiVjlMVhEaI",
-  },
-};
-
-//fetching the genre list from the api
-async function getGenresList() {
-  const genreList = await fetch(
-    "https://api.themoviedb.org/3/genre/movie/list?language=en",
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => response.genres)
-    .catch((err) => console.error(err));
-  return genreList;
-}
-//fetching the language list from the api
-async function getLanguageList() {
-  const languageList = await fetch(
-    "https://api.themoviedb.org/3/configuration/languages",
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => response)
-    .catch((err) => console.error(err));
-  return languageList;
-}
-//fetching the countries list from the api
-async function getCountriesList() {
-  const countriesList = await fetch(
-    "https://api.themoviedb.org/3/configuration/countries",
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => response)
-    .catch((err) => console.error(err));
-  return countriesList;
-}
-
-// saving the genre, countries and language array to a variable to avoid sending get requests
-const languageList = (async () => {
-  const languages = await getLanguageList().then((response) => response);
-  return languages;
-})();
-const genresList = (async () => {
-  const genres = await getGenresList().then((response) => response);
-  return genres;
-})();
-const countriesList = (async () => {
-  const countries = await getCountriesList().then((response) => response);
-  return countries;
-})();
-
-// return language name from code
-async function languageCodeToName(code) {
-  let language = await languageList;
-  for (const obj of language) {
-    if (code === obj.iso_639_1) {
-      return obj.english_name;
-    }
-  }
-}
-// return country name from code
-async function countryCodeToName(code) {
-  let language = await countriesList;
-  for (const obj of language) {
-    if (code === obj.iso_3166_1) {
-      return obj.english_name;
-    }
-  }
-}
-// return genre name from id
-async function genreIdToName(id) {
-  let genres = await genresList;
-  for (const obj of genres) {
-    if (id === obj.id) {
-      return obj.name;
-    }
-  }
-}
-
-// fetch movie details from api
-async function getMovieDetails(movieId) {
-  const baseURL = ` https://api.themoviedb.org/3/movie/${movieId}?append_to_response=similar,credits`;
-
-  let movies = await fetch(baseURL, options)
-    .then((response) => response.json())
-    .catch((err) => console.error(err));
-  return movies;
-}
-
-async function showMovieDetails(movieId) {
-  const movieDetails = await getMovieDetails(movieId);
+async function showMovieDetails(movieDetails) {
   document.title = `${movieDetails.title} - MovieBox`;
   detailsDiv.innerHTML = "";
 
@@ -143,7 +55,6 @@ async function showMovieDetails(movieId) {
     companies += companySpan.outerHTML;
   }
 
-  console.log(movieDetails);
   let div = document.createElement("div");
   div.classList = "my-4";
   div.innerHTML = `
@@ -152,8 +63,6 @@ async function showMovieDetails(movieId) {
         alt="${movieDetails.title}"
         class="object-fit-cover h-100 w-100 rounded-3"
       />
-      
-
       <div class="row justify-content-around my-5">
         <div class="col-lg-4 col-6 d-none d-md-block">
           <img
@@ -204,7 +113,7 @@ async function showMovieDetails(movieId) {
             </p>
             <p class="m-2">
                 <span class="fw-bold">Release Date: </span>
-                ${new Date(movieDetails.release_date).toDateString()}
+                ${new Date(movieDetails.release_date).toDateString().slice(4)}
             </p>
             <p class="m-2">
                 <span class="fw-bold">Language: </span>
@@ -223,18 +132,8 @@ async function showMovieDetails(movieId) {
   detailsDiv.appendChild(div);
 }
 
-async function cards(movieId) {
-  // numOfTotalPages, moviesArray
-  // if (arguments.length == 2) {
-  //   movies = moviesArray;
-  //   totalPages = numOfTotalPages;
-  // } else if (arguments.length != 0) {
-  //   totalPages = numOfTotalPages;
-  // }
-
-  const movies = await getMovieDetails(movieId).then(
-    (res) => res.similar.results
-  );
+async function cards(movies) {
+  // const movies = movieslist.similar.results;
 
   cardsContainer.innerHTML = "";
 
@@ -264,15 +163,15 @@ async function cards(movieId) {
           </a>
           <div class="card-body text-color ps-0">
             <p class="d-flex justify-content-between align-items-center text-main-muted my-0">
-              <span class="text-main-muted"> 
-                <i class="fa-regular fa-calendar text-primary me-1"></i> 
-                ${movie.release_date.split("-")[0]} 
+              <span class="text-main-muted">
+                <i class="fa-regular fa-calendar text-primary me-1"></i>
+                ${movie.release_date.split("-")[0]}
               </span>
               <span class="text-main-muted">
                 <i class="fa-regular fa-star text-primary"></i>
                 ${movie.vote_average.toFixed(1)}
-              </span> 
-              
+              </span>
+
             </p>
             <a href="details.html?id=${movie.id}">
               <h3 class="card-title h4 text-color hover d-inline-block my-2">${
@@ -280,7 +179,7 @@ async function cards(movieId) {
               }</h3>
             </a>
             <p class="card-text d-flex flex-wrap column-gap-1 text-main-muted">${genres}</p>
-            
+
           </div>
         </div>
       `;
@@ -288,23 +187,12 @@ async function cards(movieId) {
   }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
   const id = window.location.href.split("id=")[1];
-  showMovieDetails(id);
-  cards(id);
-});
+  const movieDetails = await getMovieDetails(id);
 
-//* change color mode
-const colorSchemeToggler = document.querySelector("#color-scheme-toggler");
-colorSchemeToggler.addEventListener("click", function () {
-  console.log("clicked!");
-  if (document.documentElement.classList.contains("dark")) {
-    document.documentElement.classList.remove("dark");
-    colorSchemeToggler.childNodes[1].classList.remove("fa-sun");
-    colorSchemeToggler.childNodes[1].classList.add("fa-moon");
-  } else {
-    document.documentElement.classList.add("dark");
-    colorSchemeToggler.childNodes[1].classList.remove("fa-moon");
-    colorSchemeToggler.childNodes[1].classList.add("fa-sun");
-  }
+  showMovieDetails(movieDetails);
+  movieDetails.similar.results.length != 0
+    ? cards(movieDetails.similar.results)
+    : (similarMoviesSection.style.display = "none");
 });
